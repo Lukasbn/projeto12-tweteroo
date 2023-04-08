722797d84ab254fcfc407eadca47722d356626c0
LucasBento <lubentoneves@gmail.com>
2023-04-08 10:39:10 -0300

    post/sign-up finished post/tweets finished get/tweets under development

--- app.js
+++ app.js
@@ -6,8 +6,36 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('salve familia!')
+app.post('/sign-up',(req,res)=>{
+    const {username , avatar} = req.body
+    servUsers.push({username,avatar})
+    res.send('OK')
 })
+
+app.post('/tweets',(req,res)=>{
+    const {username,tweet} = req.body
+    const registeredUser = servUsers.find((user)=> user.username === username )
+    
+    if(!registeredUser){
+        return res.send('UNAUTHORIZED')
+    }
+
+    savedTweets.splice(0,0,{username,tweet})
+    if(savedTweets.length > 10){
+        savedTweets.pop()
+    }
+
+    res.send('OK')
+})
+
+app.get('/tweets',(req,res)=>{
+    if(savedTweets.length === 0){
+        return res.send(savedTweets)
+    }
+
+})
+
+const servUsers = []
+const savedTweets = []
 
 app.listen(5000)
